56e7bdfb716d6e9852f0136dc00adc2fb7910656
Ankur Singh Tomar <ankurtomar8@outlook.com>
2021-05-07 00:20:47 +0530

    C plus plus working

---
+++ C++Problems/BasicsProblems/First.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+/* int a , b, c ;
+
+cin>> a >> b ;
+
+c=a+b;
+
+cout<<"Sum is " << c << endl; */
+
+string name ;
+
+cout << "What is your name " << endl;
+
+getline(cin,name);
+
+cout<< "Welcome Mr . " <<name;
+
+
+
+//    cout<<"Hello World!\n";
+    return 0;
+}
