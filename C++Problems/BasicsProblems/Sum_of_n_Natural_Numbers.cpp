f7fc64271e6bbca81014c843d7776d3cf4ca59df
Ankur Singh Tomar <ankurtomar8@outlook.com>
2021-05-09 16:27:47 +0530

    More Basics Program practice

---
+++ C++Problems/BasicsProblems/Sum_of_n_Natural_Numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    int sum ;
+
+    cout<<"Enter the value to where sum you need to calculate"<<endl; // welcome message
+    cin>>n; // input variable
+    
+    // logic below
+
+    sum =(n*(n+1))/2;
+
+    cout<<"The sum is  "<<sum;
+
+    return 0;
+
+
+
+}
